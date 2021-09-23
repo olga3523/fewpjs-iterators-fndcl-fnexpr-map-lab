abd53c9c2f53f1d63e1405056fd45e393c7e4e66
OLGA <olichka23.85@gmail.com>
2021-09-23 10:42:26 -0400

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return [
+    "What Does The This Keyword Mean?",
+    "What Is The Constructor OO Pattern?",
+    "Implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What Is NaN And How Can We Check For It",
+    "What Is The Difference Between StopPropagation And PreventDefault?",
+    "Immutable State And Pure Functions",
+    "What Is The Difference Between == And ===?",
+    "What Is The Difference Between Event Capturing And Bubbling?",
+    "What Is JSONP?"
+  ]
 }
